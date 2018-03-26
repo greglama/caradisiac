3aeb720968f9404b8467083a1d08996ef3b7d059
Gregoire Albizzati <gregalbizz@gmail.com>
2018-03-26 02:03:49 +0200

    The function indexArrayOfDocument now work by packet of 100

--- api/carApi.js
+++ api/carApi.js
@@ -10,7 +10,7 @@ const TYPE_NAME = "model";
 
 /**
  * @param {number} sizeOfPacket 
- * fetch all the models and send queries by packet of a defined size
+ * fetches all the models and send queries by packet of a defined size
  */
 const fetchAllModels = async(sizeOfPacket) =>{
     const brands = await getBrands();
@@ -42,7 +42,7 @@ const fetchAllModels = async(sizeOfPacket) =>{
 
 /**
  * @param {string} indexName 
- * Create an index in elasticSearch
+ * Creates an index in elasticSearch
  */
 const creatIndex = async indexName =>{
     try
@@ -55,7 +55,7 @@ const creatIndex = async indexName =>{
     {
         if(err.body.error.type === "resource_already_exists_exception")
         {
-            console.log("This index already exists, it will not be add");
+            console.log("The index '" + indexName + "' already exists, it will not be add");
         }
         else
         {
@@ -71,17 +71,25 @@ const creatIndex = async indexName =>{
  * @param {string} index 
  * @param {string} type 
  * @param {object} document 
- * Index a document in elasticSearch
+ * Indexes a document in elasticSearch
  */
-const indexDocument = async (index, type, document) =>{    
-    const result = await elasticClient.index({
-        index:index,
-        id: document.uuid,
-        type:type,
-        body:document
-    });
-
-    return result;
+const indexDocument = async (index, type, document) =>{
+    try{
+        const result = await elasticClient.index({
+            index:index,
+            id: document.uuid,
+            type:type,
+            body:document
+        });
+    
+        return result;
+    }
+    catch(err)
+    {
+        console.log("The document " + String(document.uuid) + " cannot be add...");
+        console.log(err);
+        return err;
+    }
 }
 
 /**
@@ -89,25 +97,61 @@ const indexDocument = async (index, type, document) =>{
  * @param {string} type 
  * @param {object} documents 
  * Just like indexDocument but takes an array of documents instead
+ * It indexes documents by packet of 100 (there is a limit of 200 in elasticSearch queue)
  */
 const indexArrayOfDocument = async (index, type, documents) =>{
 
-    await Promise.all(documents.map(doc => indexDocument(index, type, doc)))
+    let packetSize = 100;
+
+    let indx = 0;
+    let resultatsArray = [];
+
+    while(indx < documents.length)
+    {
+        const arrayDoc = [];
+
+        if(documents.length - indx < packetSize)
+        {
+            packetSize = documents.length - indx;
+            console.log(packetSize);
+        }
+
+        for(let i = 0; i < packetSize; i++)
+        {
+            arrayDoc.push(indexDocument(index, type, documents[indx]));
+            indx ++;
+        }
+
+        const results = await Promise.all(arrayDoc);
+
+        resultatsArray.concat(results);
+
+        console.log(String(indx) + "/" + String(documents.length - 1));
+    }
+
+    return resultatsArray;
 }
 
 /**
  * @param {string} path 
  * @param {string} data 
- * save some data in a file (the purpose here is to save the 'node-car-api' data to use it offline)
+ * saves some data in a file (the purpose here is to save the 'node-car-api' data to use it offline)
  */
 const saveFechedData = async (path, data) => await fs.writeFile(path, JSON.stringify(data));
 
+/**
+ * @param {string} path 
+ * Retrieves the saved models
+ */
+const getFetchedData = async (path) => JSON.parse(fs.readFileSync(path, "utf-8"));
+
 module.exports = {
-    indexName: INDEX_NAME,
-    typeName: TYPE_NAME,
-    fetchAllModels : fetchAllModels,
+    INDEX_NAME: INDEX_NAME,
+    TYPE_NAME: TYPE_NAME,
+    fetchAllModels: fetchAllModels,
     creatIndex: creatIndex,
-    indexDocument:indexDocument,
+    indexDocument: indexDocument,
     indexArrayOfDocument: indexArrayOfDocument,
-    saveFechedData: saveFechedData
+    saveFechedData: saveFechedData,
+    getFetchedData: getFetchedData
 };
